13d4d1638ac75931192f0430bc41e3be97149fb1
hubian <hubian1@lenovo.com>
2016-10-27 16:48:18 +0800

    --amend
    Change-Id: If97c127cf738c0c5f3066dea7bb77a9ea572c47b

--- openstack-plugins/ironic-plugin/kilo.example/rsa_objects/manager.py
+++ openstack-plugins/ironic-plugin/kilo.example/rsa_objects/manager.py
@@ -20,7 +20,7 @@ from ironic.objects import utils as obj_utils
 
 class Manager(base.IronicObject):
     dbapi = db_api.get_instance()
-    
+
     fields = {
         'id': int,
         'pod_id': obj_utils.str_or_none,
@@ -32,8 +32,8 @@ class Manager(base.IronicObject):
         'model': obj_utils.str_or_none,
         'firmware_version': obj_utils.str_or_none,
         'graphical_console': obj_utils.str_or_none,
-        'serial_console':  obj_utils.str_or_none,
-        'command_shell':  obj_utils.str_or_none,
+        'serial_console': obj_utils.str_or_none,
+        'command_shell': obj_utils.str_or_none,
         'status': obj_utils.str_or_none,
     }
 
@@ -47,113 +47,46 @@ class Manager(base.IronicObject):
 
     @base.remotable_classmethod
     def get_by_id(cls, context, manager_id):
-        """Find a manager based on its integer id and return a Node details info including cpu memory and disk.
-
-        :param manager_id: the id of a manager.
-        :returns: a :class:`Node` object.
-        """
         db_manager = cls.dbapi.get_manager_by_id(manager_id)
         return Manager._from_db_object(cls(context), db_manager)
 
     @base.remotable_classmethod
     def get_by_url(cls, context, url):
-        """Find a manager based on url and return a Node object.
-
-        :param url: the url of a manager.
-        :returns: a :class:`Node` object.
-        """
         db_manager = cls.dbapi.get_manager_by_url(url)
         manager = Manager._from_db_object(cls(context), db_manager)
         return manager
 
     @base.remotable_classmethod
-    def list_by_pod(cls, context, pod_id, limit=None, marker=None, sort_key=None, sort_dir=None, ):
-        """Return a list of Node objects.filter_by pod_id
-
-        :param pod_id: which pod scope
-        :param context: Security context.
-        :param limit: maximum number of resources to return in a single result.
-        :param marker: pagination marker for large data sets.
-        :param sort_key: column to sort results by.
-        :param sort_dir: direction to sort. "asc" or "desc".
-        :param filters: Filters to apply.
-        :returns: a list of :class:`Manager` object.
-
-        """
+    def list_by_pod(cls, context, pod_id, limit=None, marker=None,
+                    sort_key=None, sort_dir=None, ):
         db_managers = cls.dbapi.get_manager_list(pod_id=pod_id,
-                                                      limit=limit,
-                                                      marker=marker,
-                                                      sort_key=sort_key,
-                                                      sort_dir=sort_dir)
-        return [Manager._from_db_object(cls(context), obj) for obj in db_managers]
+                                                 limit=limit,
+                                                 marker=marker,
+                                                 sort_key=sort_key,
+                                                 sort_dir=sort_dir)
+        return [Manager._from_db_object(cls(context), obj) for obj in
+                db_managers]
 
     @base.remotable
     def create(self, context=None):
-        """Create a Node record in the DB.
-
-        Column-wise updates will be made based on the result of
-        self.what_changed(). If target_power_state is provided,
-        it will be checked against the in-database copy of the
-        manager before updates are made.
-
-        :param context: Security context. NOTE: This should only
-                        be used internally by the indirection_api.
-                        Unfortunately, RPC requires context as the first
-                        argument, even though we don't use it.
-                        A context should be set when instantiating the
-                        object, e.g.: Node(context)
-
-        """
         values = self.obj_get_changes()
         db_manager = self.dbapi.create_manager(values)
         self._from_db_object(self, db_manager)
 
     @base.remotable
     def save(self, context=None):
-        """Save updates to this Node.
-
-        Column-wise updates will be made based on the result of
-        self.what_changed(). If target_power_state is provided,
-        it will be checked against the in-database copy of the
-        manager before updates are made.
-
-        :param context: Security context. NOTE: This should only
-                        be used internally by the indirection_api.
-                        Unfortunately, RPC requires context as the first
-                        argument, even though we don't use it.
-                        A context should be set when instantiating the
-                        object, e.g.: Node(context)
-        """
         updates = self.obj_get_changes()
         self.dbapi.update_manager(self.id, updates)
         self.obj_reset_changes()
 
     @base.remotable
     def refresh(self, context=None):
-        """Refresh the object by re-fetching from the DB.
-
-        :param context: Security context. NOTE: This should only
-                        be used internally by the indirection_api.
-                        Unfortunately, RPC requires context as the first
-                        argument, even though we don't use it.
-                        A context should be set when instantiating the
-                        object, e.g.: Node(context)
-        """
-        current = self.__class__.get_by_url(self._context, self.url)
+        current = self.__class__.get_by_id(self._context, self.id)
         for field in self.fields:
-            if (hasattr(self, base.get_attrname(field)) and
-                        self[field] != current[field]):
-                self[field] = current[field]
+            if hasattr(self, base.get_attrname(field)):
+                if self[field] != current[field]:
+                    self[field] = current[field]
 
     @base.remotable_classmethod
     def destroy(cls, pod_id, context=None):
-        """Delete the driver_instance_server from the DB.
-
-        :param context: Security context. NOTE: This should only
-                        be used internally by the indirection_api.
-                        Unfortunately, RPC requires context as the first
-                        argument, even though we don't use it.
-                        A context should be set when instantiating the
-                        object, e.g.: driver_instance_server(context)
-        """
         cls.dbapi.destroy_manager(pod_id)
--- openstack-plugins/ironic-plugin/kilo.example/rsa_podm/power.py
+++ openstack-plugins/ironic-plugin/kilo.example/rsa_podm/power.py
@@ -19,11 +19,13 @@ RSA Pod Manager Power Driver using RSA PODM
 from ironic.common import states
 from ironic.conductor import task_manager
 from ironic.drivers import base
-from oslo_log import log as logging
-from ironic.drivers.modules.rsa_podm import rsa_podm_adapter as pod_manager
+
 from ironic.drivers.modules.rsa_podm import common as rsa_pod_common
 from ironic.drivers.modules.rsa_podm import constants
+from ironic.drivers.modules.rsa_podm import rsa_podm_adapter as pod_manager
 
+
+from oslo_log import log as logging
 
 LOG = logging.getLogger(__name__)
 
@@ -38,13 +40,9 @@ class RSAPodmPower(base.PowerInterface):
         return rsa_pod_common.parse_podm_driver_info(task.node)
 
     def get_power_state(self, task):
-        """
-        get power state by Pod Manager, exception handling needed
-        since we support multi Pod Manager, each node's driver info may have different xClarity info
-        :param task: 
-        """
         # only when we need to deploy, we use the context to add node into DB
-        pod_manager_obj = pod_manager.get_podm_connection_by_node(None, task.node)
+        pod_manager_obj = pod_manager.get_podm_connection_by_node(None,
+                                                                  task.node)
         uuid = task.node.extra['physical_uuid']
         power_state = pod_manager_obj.get_power_state(uuid)
         if power_state == 'unknown':
@@ -54,12 +52,8 @@ class RSAPodmPower(base.PowerInterface):
 
     @task_manager.require_exclusive_lock
     def set_power_state(self, task, power_state):
-        """
-        set power state by xClarity, exception handling needed
-        :param power_state: 
-        :param task: 
-        """
-        pod_manager_obj = pod_manager.get_podm_connection_by_node(None, task.node)
+        pod_manager_obj = pod_manager.get_podm_connection_by_node(None,
+                                                                  task.node)
         uuid = task.node.extra['physical_uuid']
 
         if power_state == states.POWER_ON:
@@ -70,10 +64,7 @@ class RSAPodmPower(base.PowerInterface):
 
     @task_manager.require_exclusive_lock
     def reboot(self, task):
-        """
-        reboot server by xClarity, exception handling needed
-        :param task:
-        """
-        pod_manager_obj = pod_manager.get_podm_connection_by_node(None, task.node)
+        pod_manager_obj = pod_manager.get_podm_connection_by_node(None,
+                                                                  task.node)
         uuid = task.node.extra['physical_uuid']
         pod_manager_obj.set_power_state(uuid, constants.PODM_POWER_REBOOT_CMD)
