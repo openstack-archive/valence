0ef0ef222cdd2a753eb131c5f5f3e1584d4a58ac
hubian <hubian1@lenovo.com>
2016-10-27 16:48:18 +0800

    --amend
    Change-Id: If97c127cf738c0c5f3066dea7bb77a9ea572c47b

--- openstack-plugins/ironic-plugin/kilo.example/rsa_objects/interface.py
+++ openstack-plugins/ironic-plugin/kilo.example/rsa_objects/interface.py
@@ -47,15 +47,11 @@ class Interface(base.IronicObject):
     @staticmethod
     def _from_db_object_list(db_objects, cls, context):
         """Converts a list of database entities to a list of formal objects."""
-        return [Interface._from_db_object(cls(context), obj) for obj in db_objects]
+        return [Interface._from_db_object(cls(context), obj) for obj in
+                db_objects]
 
     @base.remotable_classmethod
     def get(cls, context, interface_id):
-        """Find a interface based on its id or uuid and return a interface object.
-
-        :param interface_id: the id *or* uuid of a interface.
-        :returns: a :class:`interface` object.
-        """
         if strutils.is_int_like(interface_id):
             return cls.get_by_id(context, interface_id)
         elif uuidutils.is_uuid_like(interface_id):
@@ -67,64 +63,32 @@ class Interface(base.IronicObject):
 
     @base.remotable_classmethod
     def get_by_id(cls, context, interface_id):
-        """Find a interface based on its integer id and return a interface object.
-
-        :param interface_id: the id of a interface.
-        :returns: a :class:`interface` object.
-        """
         db_interface = cls.dbapi.get_interface_by_id(interface_id)
         interface = Interface._from_db_object(cls(context), db_interface)
         return interface
 
     @base.remotable_classmethod
-    def list_by_node_id(cls, context, node_id, limit=None, marker=None, sort_key=None, sort_dir=None):
-        db_interface = cls.dbapi.get_node_interface_list(node_id, limit=limit, marker=marker, sort_key=sort_key,
+    def list_by_node_id(cls, context, node_id, limit=None, marker=None,
+                        sort_key=None, sort_dir=None):
+        db_interface = cls.dbapi.get_node_interface_list(node_id, limit=limit,
+                                                         marker=marker,
+                                                         sort_key=sort_key,
                                                          sort_dir=sort_dir)
-        return [Interface._from_db_object(cls(context), obj) for obj in db_interface]
+        return [Interface._from_db_object(cls(context), obj) for obj in
+                db_interface]
 
     @base.remotable
     def create(self, context=None):
-        """Create a interface record in the DB.
-
-        :param context: Security context. NOTE: This should only
-                        be used internally by the indirection_api.
-                        Unfortunately, RPC requires context as the first
-                        argument, even though we don't use it.
-                        A context should be set when instantiating the
-                        object, e.g.: interface(context)
-
-        """
         values = self.obj_get_changes()
         db_interface = self.dbapi.create_interface(values)
         self._from_db_object(self, db_interface)
 
     @base.remotable_classmethod
     def destroy(cls, computer_system_id, context=None):
-        """Delete the interface from the DB.
-
-        :param context: Security context. NOTE: This should only
-                        be used internally by the indirection_api.
-                        Unfortunately, RPC requires context as the first
-                        argument, even though we don't use it.
-                        A context should be set when instantiating the
-                        object, e.g.: interface(context)
-        """
         cls.dbapi.destroy_interface(computer_system_id)
 
     @base.remotable
     def save(self, context=None):
-        """Save updates to this interface.
-
-        Updates will be made column by column based on the result
-        of self.what_changed().
-
-        :param context: Security context. NOTE: This should only
-                        be used internally by the indirection_api.
-                        Unfortunately, RPC requires context as the first
-                        argument, even though we don't use it.
-                        A context should be set when instantiating the
-                        object, e.g.: interface(context)
-        """
         updates = self.obj_get_changes()
         self.dbapi.update_interface(self.id, updates)
         self.obj_reset_changes()
