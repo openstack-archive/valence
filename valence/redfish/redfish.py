341f79057a256e7410a2790aa1f4997989b5260e
Seenivasan Gunabalan <agsnivas@gmail.com>
2016-11-24 20:38:42 +0530

    Added Error Response
    Added Formating Error response and Confirmation Messages.
    When an operation is successfull, its better to send confirmation
    message in a standard format. Also when Errors are at Valence-PODM
    connection it should be handled and returned as json as per schema
    referred in openstack
    
    Change-Id: Ic440caa2a1865ffa5dc95e08ad398e0ad58861a7
    Closes-Bug: #1643123

--- valence/redfish/redfish.py
+++ valence/redfish/redfish.py
@@ -21,6 +21,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 from valence import config as cfg
+from valence.common import exception
 from valence.redfish import tree
 
 
@@ -272,7 +273,10 @@ def get_systembyid(systemid):
 
 
 def get_nodebyid(nodeid):
-    return nodes_list({"Id": nodeid})
+    node = nodes_list({"Id": nodeid})
+    if not node:
+        raise exception.NotFound()
+    return node[0]
 
 
 def build_hierarchy_tree():
@@ -297,19 +301,23 @@ def compose_node(data):
     composeurl = "Nodes/Actions/Allocate"
     headers = {'Content-type': 'application/json'}
     criteria = data["criteria"]
-    if not criteria:
-        resp = send_request(composeurl, "POST", headers=headers)
+    resp = send_request(composeurl, "POST", json=criteria, headers=headers)
+    if resp.status_code == 201:
+        composednode = resp.headers['Location']
+        return {"node": composednode}
     else:
-        resp = send_request(composeurl, "POST", json=criteria, headers=headers)
-
-    composednode = resp.headers['Location']
-    return {"node": composednode}
+        raise exception.RedfishException(resp.json(),
+                                         status_code=resp.status_code)
 
 
 def delete_composednode(nodeid):
     deleteurl = "Nodes/" + str(nodeid)
     resp = send_request(deleteurl, "DELETE")
-    return resp
+    if resp.status_code == 204:
+        return exception.confirmation("", "DELETED"), resp.status_code
+    else:
+        raise exception.RedfishException(resp.json(),
+                                         status_code=resp.status_code)
 
 
 def nodes_list(filters={}):
