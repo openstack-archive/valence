5e35d0ea90dc04de7e76e4f1e9f11f53ec893e4f
hubian <hubian1@lenovo.com>
2016-11-25 16:39:04 +0800

    valence ironic plugin
    Change-Id: Ie641b4d177e1ee85143f0e46127e459c708719f4

---
+++ openstack-plugins/ironic-plugin/drivers/modules/valence/management.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+from ironic.drivers import base
+from oslo_log import log as logging
+
+LOG = logging.getLogger(__name__)
+
+class ValenceManagement(base.ManagementInterface):
+
+    def get_properties(self):
+        pass
+
+    def validate(self, task):
+        pass
+
+    def get_supported_boot_devices(self, task):
+        pass    
+
+    def set_boot_device(self, task, device, persistent=False):
+        pass
+
+    def get_boot_device(self, task):
+        pass
+
+    def get_sensors_data(self, task):
+        pass
+
