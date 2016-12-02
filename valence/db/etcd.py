2f9e9e564a4436a7dd3659ed627014807ece8565
hubian <hubian1@lenovo.com>
2016-12-02 10:33:03 +0800

    Insert DB:etcd for BP:multi-podmanager
    This patch adds etcd DB for multi-podmanager as well as valence.
    
    Change-Id: Ie1ef2554cc95a5f9f18341e768f27a02e192b588
    Closes-Bug: #1646389

---
+++ valence/db/etcd.py
@@ -0,0 +1,34 @@
+# Copyright 2016 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+etcd_client = None
+
+def setup_db():
+    pass
+
+
+class EtcdAdapter():
+
+    def add_object(dir, object):
+        pass
+
+    def update_object(dir, uuid, **update_itesm):
+        pass
+
+    def get_object_list(dir):
+        pass
+
+    def get_object_by_uuid(dir, uuid):
+        pass
