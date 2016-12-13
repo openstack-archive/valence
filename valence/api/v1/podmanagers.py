dc9648235fcf869ab18812de8295d0baf30c3f64
hubian <hubian1@lenovo.com>
2016-12-13 14:09:33 +0800

    implemente pod_manager controller for BP:multi-podmanager
    Change-Id: I1c50e336f8dc85253c8bdd3611cccc11ef1c1300

---
+++ valence/api/v1/podmanagers.py
@@ -0,0 +1,49 @@
+# Copyright (c) 2016 Intel, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import logging
+
+from flask import request
+from flask_restful import Resource
+
+from valence.podmanagers import podmanagers
+
+LOG = logging.getLogger(__name__)
+
+
+class PodManagersList(Resource):
+    def get(self):
+        LOG.debug("GET /pod_managers")
+        return podmanagers.get_podm_list()
+
+
+    def post(self):
+        LOG.debug("POST /pod_managers")
+        podm_items = request.get_json()
+        return podmanagers.create_podm(**podm_items)
+
+
+class PodManager(Resource):
+    def get(self, podm_id):
+        LOG.debug("GET /pod_managers/" + podm_id)
+        return podmanagers.get_podm_by_uuid(podm_id)
+
+    def patch(self, podm_id):
+        LOG.debug("PATCH /pod_managers/" + podm_id)
+        podm_items = request.get_json()
+        return podmanagers.update_podm(podm_id, **podm_items)
+
+    def delete(self, podm_id):
+        LOG.debug("DELETE /pod_managers/" + podm_id)
+        return podmanagers.delete_podm_by_uuid(podm_id)
---
+++ valence/podmanagers/podmanagers.py
@@ -0,0 +1,64 @@
+# Copyright (c) 2016 Intel, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from valence.db.etcd_adapter import EtcdAdapter
+from datetime import datetime
+import requests
+
+
+def _check(items, status=None):
+    # check not null
+    fileds = ['name', 'url', 'auth']
+    values = [i.value for i in items if i in fileds]
+    if not all(values):
+        return "please check your args, 'name/url/auth/' can't be null"
+
+    # check url status
+    try:
+        requests.get(items['url'], auth=items['auth'])
+        status = 'Online'
+    except requests.ConnectionError:
+        status = 'Offline'
+
+    return True, status
+
+
+def get_podm_list():
+    return EtcdAdapter.get_object_list("/v1/podmanagers")
+
+
+def get_podm_by_uuid(uuid):
+    return EtcdAdapter.get_object_by_uuid('/v1/pod_managers', uuid)
+
+
+def update_podm(uuid, **update_items):
+    key = '/v1/pod_managers/' + uuid
+    flag, status = _check(update_items)
+    if flag:
+        update_items.update({'update_at': str(datetime.now()), 'status': status})
+        return EtcdAdapter.update_object(key, **update_items)
+
+
+def create_podm(**podm_items):
+    flag, status = _check(podm_items)
+    if flag:
+        e = EtcdAdapter()
+        podm_items.update({'create_at': str(datetime.now()), 'status': status})
+        return e.add_object("/v1/podmanagers", **podm_items)
+
+
+def delete_podm_by_uuid(uuid):
+    return EtcdAdapter.delete_object_by_uuid('/v1/pod_managers', uuid)
+
+
