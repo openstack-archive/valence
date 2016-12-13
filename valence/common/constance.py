44b3170a66e7bdeb73e70d4a816d036f62304d1d
hubian <hubian1@lenovo.com>
2016-12-13 14:09:33 +0800

    implement pod_manager controller for BP:multi-podmanager
    Change-Id: I1c50e336f8dc85253c8bdd3611cccc11ef1c1300
    Closes-Bug: #1646390

---
+++ valence/common/constance.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2016 Intel, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+PODM_AUTH_BASIC_TYPE = 'basic'
+
+PODM_STATUS_ONLINE = 'Online'
+PODM_STATUS_OFFLINE = 'Offline'
+PODM_STATUS_UNKNOWN = "Unknown"
