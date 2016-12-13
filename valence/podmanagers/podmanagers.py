d87f21871ba77118524783101d25dd4e12623713
hubian <hubian1@lenovo.com>
2016-12-13 14:09:33 +0800

    implement pod_manager controller for BP:multi-podmanager
    Change-Id: I1c50e336f8dc85253c8bdd3611cccc11ef1c1300
    Closes-Bug: #1646390

---
+++ valence/podmanagers/podmanagers.py
@@ -0,0 +1,107 @@
+# Copyright (c) 2016 Intel, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import requests
+
+from valence.common import constance
+from valence.common.exception import NotFound, BadRequest
+from valence.db.api import get_connection
+
+_db_connection = get_connection()
+
+
+def _check_creation(values):
+    """Checking args when creating a new pod manager
+
+        name: can not be duplicated
+        url: can not be duplicated
+
+    :values: The properties for this new pod manager.
+    :returns: improved values that could be inserted to db
+    """
+
+    pod_manager_list = get_podm_list()
+    names = map(lambda x: x['name'], pod_manager_list)
+    urls = map(lambda x: x['url'], pod_manager_list)
+    if values['name'] in names or values['url'] in urls:
+        raise BadRequest
+
+    # input status
+    values['status'] = get_podm_status(values['url'], values['authentication'])
+
+    return values
+
+
+def _check_updation(uuid, values):
+    """Checking args when updating a exist pod manager
+
+    :values: The properties of pod manager to be updated
+    :returns: improved values that could be updated
+    """
+
+    pod_manager = get_podm_by_uuid(uuid)
+    if pod_manager is None:
+        raise NotFound
+
+    # uuid, url can not be modified
+    if 'uuid' in values:
+        values.pop('uuid')
+    if 'url' in values:
+        values.pop('url')
+    return values
+
+
+def get_podm_list():
+    return map(lambda x: x.as_dict(), _db_connection.list_podmanager())
+
+
+def get_podm_by_uuid(uuid):
+    return _db_connection.get_podmanager_by_uuid(uuid).as_dict()
+
+
+def create_podm(values):
+    values = _check_creation(values)
+    return _db_connection.create_podmanager(values).as_dict()
+
+
+def update_podm(uuid, values):
+    values = _check_updation(uuid, values)
+    return _db_connection.update_podmanager(uuid, values).as_dict()
+
+
+def delete_podm_by_uuid(uuid):
+    # TODO(hubian) this need to break the links between podm and its Nodes
+    return _db_connection.delete_podmanager(uuid)
+
+
+def get_podm_status(url, authentication):
+    """get pod manager running status by its url and auth
+
+    :param url: The url of pod manager.
+    :param authentication: array, The auth(s) info of pod manager.
+
+    :returns: status of the pod manager
+    """
+    for auth in authentication:
+        try:
+            # TODO(Hubian) Only consider and support basic auth type here.
+            # After decided to support other auth type this would be improved.
+            if auth['type'] == constance.PODM_AUTH_BASIC_TYPE:
+                username = auth['auth_items']['username']
+                password = auth['auth_items']['password']
+                requests.get(url, auth=(username, password))
+                return constance.PODM_STATUS_ONLINE
+        except requests.ConnectionError:
+            return constance.PODM_STATUS_OFFLINE
+    return constance.PODM_STATUS_UNKNOWN
---
+++ valence/tests/unit/podmanagers/test_podmanagers.py
@@ -0,0 +1,155 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import copy
+import mock
+import requests
+import unittest
+
+from valence.common import constance
+from valence.common.exception import NotFound, BadRequest
+from valence.podmanagers import podmanagers
+
+
+class TestPodManagers(unittest.TestCase):
+    @mock.patch('valence.podmanagers.podmanagers.get_podm_list')
+    @mock.patch('valence.podmanagers.podmanagers.get_podm_status')
+    def test_check_creation(self, mock_get_podm_status, mock_get_podm_list):
+        mock_get_podm_list.return_value = [
+            {"name": "test1",
+             "url": "https://10.0.0.1"},
+            {"name": "test2",
+             "url": "https://10.0.0.2"}
+        ]
+        mock_get_podm_status.return_value = constance.PODM_STATUS_ONLINE
+
+        values = {"name": "podm_name",
+                  "url": "https://10.240.212.123",
+                  "authentication": [
+                      {
+                          "type": "basic",
+                          "auth_items": {
+                              "username": "xxxxxxx",
+                              "password": "xxxxxxx"
+                          }
+                      }
+                  ]}
+
+        result_values = copy.deepcopy(values)
+        result_values['status'] = constance.PODM_STATUS_ONLINE
+
+        self.assertEqual(podmanagers._check_creation(values), result_values)
+        mock_get_podm_status.assert_called_once_with(values['url'],
+                                                     values['authentication'])
+        mock_get_podm_list.assert_called_once_with()
+
+    @mock.patch('valence.podmanagers.podmanagers.get_podm_list')
+    def test_check_creation_duplicate_Exception(self, mock_get_podm_list):
+        mock_get_podm_list.return_value = [
+            {"name": "test1",
+             "url": "https://10.0.0.1"},
+            {"name": "test2",
+             "url": "https://10.0.0.2"}
+        ]
+
+        name_duplicate_values = {"name": "test1",
+                                 "url": "https://10.240.212.123"}
+
+        url_duplicate_values = {"name": "podm_name",
+                                "url": "https://10.0.0.2"}
+
+        self.assertRaises(BadRequest,
+                          podmanagers._check_creation,
+                          name_duplicate_values)
+        self.assertRaises(BadRequest,
+                          podmanagers._check_creation,
+                          url_duplicate_values)
+        self.assertEqual(mock_get_podm_list.call_count, 2)
+
+    @mock.patch('valence.podmanagers.podmanagers.get_podm_by_uuid')
+    def test_check_updation_resource_not_found(self, mock_get_podm_by_uuid):
+        mock_get_podm_by_uuid.return_value = None
+        self.assertRaises(NotFound,
+                          podmanagers._check_updation,
+                          "uuid", {})
+        mock_get_podm_by_uuid.assert_called_once_with("uuid")
+
+    @mock.patch('valence.podmanagers.podmanagers.get_podm_by_uuid')
+    def test_check_updation_ignore_url_uuid(self, mock_get_podm_by_uuid):
+        mock_get_podm_by_uuid.return_value = {}
+
+        values = {"uuid": "uuid",
+                  "url": "url",
+                  "name": "name"}
+        result_values = copy.deepcopy(values)
+        result_values.pop('url')
+        result_values.pop('uuid')
+
+        self.assertEqual(podmanagers._check_updation('uuid', values),
+                         result_values)
+        mock_get_podm_by_uuid.assert_called_once_with("uuid")
+
+    @mock.patch('requests.get')
+    def test_get_podm_status_Offline(self, mock_get):
+        mock_get.side_effect = requests.ConnectionError
+        authentication = [
+            {
+                "type": "basic",
+                "auth_items": {
+                    "username": "xxxxxxx",
+                    "password": "xxxxxxx"
+                }
+            }
+        ]
+        auth = ("xxxxxxx", "xxxxxxx")
+
+        self.assertEqual(podmanagers.get_podm_status('url', authentication),
+                         constance.PODM_STATUS_OFFLINE)
+        mock_get.asset_called_once_with('url', auth)
+
+    def test_get_podm_status_unknown(self):
+        """not basic type authentication podm status set value to be unknown"""
+        authentication = [
+            {
+                "type": "CertificateAuthority",
+                "auth_items": {
+                    "public_key": "xxxxxxx"
+                }
+            },
+            {
+                "type": "DynamicCode",
+                "auth_items": {
+                    "code": "xxxxxxx"
+                }
+            }
+
+        ]
+        self.assertEqual(podmanagers.get_podm_status('url', authentication),
+                         constance.PODM_STATUS_UNKNOWN)
+
+    @mock.patch('requests.get')
+    def test_get_podm_status_Online(self, mock_get):
+        mock_get.return_value = 'ok'
+        authentication = [
+            {
+                "type": "basic",
+                "auth_items": {
+                    "username": "xxxxxxx",
+                    "password": "xxxxxxx"
+                }
+            }
+        ]
+        auth = ("xxxxxxx", "xxxxxxx")
+
+        self.assertEqual(podmanagers.get_podm_status('url', authentication),
+                         constance.PODM_STATUS_ONLINE)
+        mock_get.asset_called_once_with('url', auth)
