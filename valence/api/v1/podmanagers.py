26b03bbecb377b6bf5c4329645f56f89c0fe8c04
hubian <hubian1@lenovo.com>
2016-12-13 14:09:33 +0800

    implement pod_manager controller for BP:multi-podmanager
    Change-Id: I1c50e336f8dc85253c8bdd3611cccc11ef1c1300
    Closes-Bug: #1646390

---
+++ valence/api/v1/podmanagers.py
@@ -0,0 +1,51 @@
+# Copyright (c) 2016 Intel, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import logging
+
+import flask
+import flask_restful
+from six.moves import http_client
+
+from valence.api.podmanagers import podmanagers
+from valence.common import utils
+
+
+LOG = logging.getLogger(__name__)
+
+
+class PodManagersList(flask_restful.Resource):
+
+    def get(self):
+        return utils.make_response(http_client.OK, podmanagers.get_podm_list())
+
+    def post(self):
+        values = flask.request.get_json()
+        return utils.make_response(http_client.OK,
+                                   podmanagers.create_podm(values))
+
+
+class PodManager(flask_restful.Resource):
+
+    def get(self, podm_uuid):
+        return utils.make_response(http_client.OK,
+                                   podmanagers.get_podm_by_uuid(podm_uuid))
+
+    def patch(self, podm_uuid):
+        values = flask.request.form.to_dict()
+        return utils.make_response(http_client.OK,
+                                   podmanagers.update_podm(podm_uuid, values))
+
+    def delete(self, podm_uuid):
+        podmanagers.delete_podm_by_uuid(podm_uuid)
