1f27d057fc230450721447a5b60661dd2f4ac186
Nate Potter <nathaniel.potter@intel.com>
2016-12-21 16:04:34 -0800

    Add Rack and Pod APIs
    This commit adds APIs and corresponding controller functions
    for listing pod and rack resources.
    
    Change-Id: I66fd2f9cfe30b7a87ee4ea16a9f8027c34d1d0e6
    Closes-bug: #1633443

---
+++ valence/api/v1/racks.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2016 Intel, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import logging
+
+from flask import request
+from flask_restful import Resource
+
+from valence.redfish import redfish
+
+LOG = logging.getLogger(__name__)
+
+
+class RackList(Resource):
+
+    def get(self):
+        LOG.debug("GET /v1/racks")
+        return redfish.list_racks(request.args)
+
+
+class Rack(Resource):
+
+    def get(self, rack_id):
+        LOG.debug("GET /v1/racks/" + rack_id)
+        return redfish.show_rack(rack_id)
