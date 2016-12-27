f32b9c99fa7f5495768e1d9a276309c11a114f60
Yufei <zhang.yufei@99cloud.net>
2016-12-27 18:14:28 +0800

    use integer status code instead of string
    there are many string status_code in test_redfish.py, unittest does
    not report a error becasue they are never used, but it should be
    integer in fact.
    
    Also replace some magic number with httpclient code.
    
    Change-Id: I7897a5e9ef91cc1a601adee8883f6ee46e11add8

--- valence/tests/functional/test_functional.py
+++ valence/tests/functional/test_functional.py
@@ -1,3 +1,5 @@
+from six.moves import http_client
+
 from valence.tests import FunctionalTest
 
 
@@ -5,8 +7,8 @@ class TestRootController(FunctionalTest):
 
     def test_root_get(self):
         response = self.app.get('/')
-        assert response.status_code == 200
+        assert response.status_code == http_client.OK
 
     def test_v1_get(self):
         response = self.app.get('/v1')
-        assert response.status_code == 200
+        assert response.status_code == http_client.OK
