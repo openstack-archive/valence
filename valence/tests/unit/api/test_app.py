3f38358ac5b812bfdce6426bcd35507039b044e0
Andy Yan <yanchao3@lenovo.com>
2016-12-28 16:41:29 +0800

    Add basic unit test for api/app
    Add basic unit test for api/app
    
    Change-Id: I400a3fbce227883738feb9c07d9328928fc17956
    Signed-off-by: Andy Yan <yanchao3@lenovo.com>

---
+++ valence/tests/unit/api/test_app.py
@@ -0,0 +1,33 @@
+# copyright (c) 2016 Intel, Inc.
+#
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+import unittest
+
+from valence.api import app
+
+
+class Test_App(unittest.TestCase):
+
+    @mock.patch('valence.config.PROJECT_NAME')
+    @mock.patch('valence.api.app.Flask')
+    def test_setup_app_success(self, mock_Flask, mock_PROJECT_NAME):
+        self.app = app.setup_app()
+        mock_Flask.assert_called_with(mock_PROJECT_NAME)
+        self.assertFalse(self.app.url_map.strict_slashes)
+
+    @mock.patch('valence.api.app.setup_app')
+    def test_get_app_success(self, mock_setup_app):
+        self._app = app.get_app()
+        self.assertNotEqual(self._app, None)
+
