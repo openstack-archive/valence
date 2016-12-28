6af00d9a44c7442401a6939fcbc6ea008c48220d
Maohaijun <maohj2@lenovo.com>
2016-12-28 16:28:54 +0800

    add unit tests for files api.link and api.types
    Change-Id: I4b1506d01a5883d6091f4405ae396440309c2d7d

---
+++ valence/tests/unit/api/test_link.py
@@ -0,0 +1,40 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from unittest import TestCase
+
+from valence.api import link as link_module
+
+
+class TestLink(TestCase):
+    def test_build_url_with_bookmark(self):
+        link = link_module.build_url(
+            'v1', 'flavors', bookmark=True, base_url='http://localhost:8181')
+
+        self.assertEqual('http://localhost:8181/v1/flavors', link)
+
+    def test_build_url_without_bookmark(self):
+        link = link_module.build_url(
+            '', 'flavors', bookmark=False, base_url='http://localhost:8181')
+
+        self.assertEqual('http://localhost:8181/v1//flavors', link)
+
+    def test_make_link(self):
+        link = link_module.Link.make_link(
+            'self', 'http://localhost:8181', 'v1', '',
+            bookmark=True)
+
+        expected_value = {
+            'href': 'http://localhost:8181/v1/',
+            'rel': 'self'
+        }
+        self.assertEqual(expected_value, link.as_dict())
---
+++ valence/tests/unit/api/test_types.py
@@ -0,0 +1,146 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from unittest import TestCase
+
+from valence.api import types
+from valence.common import base
+
+
+class TestTypes(TestCase):
+    def test_text(self):
+        self.assertIsNone(types.Text.validate(None))
+
+        self.assertEqual('test_value', types.Text.validate('test_value'))
+        self.assertRaises(ValueError,
+                          types.Text.validate, 10)
+
+    def test_string_type(self):
+        self.assertIsNone(types.String.validate(None))
+
+        test_value = 'test_value'
+        self.assertEqual(test_value, types.String.validate(test_value))
+        self.assertRaises(ValueError,
+                          types.String.validate, 10)
+
+        length = len(test_value)
+        # test min_length
+        self.assertEqual(test_value, types.String.validate(
+            test_value, min_length=length - 1))
+        self.assertEqual(test_value, types.String.validate(
+            test_value, min_length=length))
+        self.assertRaises(ValueError,
+                          types.String.validate, test_value,
+                          min_length=length + 1)
+
+        # test max_length
+        self.assertRaises(ValueError,
+                          types.String.validate, test_value,
+                          max_length=length - 1)
+        self.assertEqual(test_value, types.String.validate(
+            test_value, max_length=length))
+        self.assertEqual(test_value, types.String.validate(
+            test_value, max_length=length + 1))
+
+    def test_integer_type(self):
+        self.assertIsNone(types.Integer.validate(None))
+
+        test_value = 10
+        self.assertEqual(test_value, types.Integer.validate(test_value))
+        self.assertEqual(test_value, types.Integer.validate('10'))
+        self.assertRaises(ValueError,
+                          types.Integer.validate, 'invalid')
+        self.assertRaises(ValueError,
+                          types.Integer.validate, '0.5')
+
+        # test minimum
+        self.assertEqual(test_value, types.Integer.validate(
+            test_value, minimum=9))
+        self.assertEqual(test_value, types.Integer.validate(
+            test_value, minimum=10))
+        # self.assertRaises(test_value, types.Integer.validate(
+        #    test_value, minimum=11))
+
+    def test_bool_type(self):
+        self.assertTrue(types.Bool.validate(None, default=True))
+
+        test_value = True
+        self.assertEqual(test_value, types.Bool.validate(True))
+        self.assertEqual(test_value, types.Bool.validate('True'))
+        self.assertEqual(test_value, types.Bool.validate('true'))
+        self.assertEqual(test_value, types.Bool.validate('t'))
+        self.assertEqual(test_value, types.Bool.validate('yes'))
+        self.assertEqual(test_value, types.Bool.validate('1'))
+
+        test_value = False
+        self.assertEqual(test_value, types.Bool.validate(False))
+        self.assertEqual(test_value, types.Bool.validate('False'))
+        self.assertEqual(test_value, types.Bool.validate('TTT'))
+        self.assertEqual(test_value, types.Bool.validate(''))
+        self.assertEqual(test_value, types.Bool.validate('0'))
+        self.assertRaises(ValueError,
+                          types.Bool.validate, None)
+        self.assertRaises(ValueError,
+                          types.Bool.validate, 0)
+
+    def test_custom(self):
+        class TestAPI(base.ObjectBase):
+            fields = {
+                'test': {
+                    'validate': lambda v: v
+                },
+            }
+
+        test_type = types.Custom(TestAPI)
+        self.assertIsNone(test_type.validate(None))
+
+        value = TestAPI(test='test_value')
+        value = test_type.validate(value)
+        self.assertIsInstance(value, TestAPI)
+        self.assertEqual({'test': 'test_value'}, value.as_dict())
+
+        test_type = types.Custom(TestAPI)
+        value = test_type.validate({'test': 'test_value'})
+        self.assertIsInstance(value, TestAPI)
+        self.assertEqual({'test': 'test_value'}, value.as_dict())
+
+        self.assertRaises(
+            ValueError,
+            test_type.validate, 'invalid_value')
+
+    def test_list_with_text_type(self):
+        list_type = types.List(types.Text)
+        self.assertIsNone(list_type.validate(None))
+
+        value = list_type.validate(['test1', 'test2'])
+        self.assertEqual(['test1', 'test2'], value)
+
+        self.assertRaises(
+            ValueError,
+            list_type.validate, 'invalid_value')
+
+    def test_list_with_custom_type(self):
+        class TestAPI(base.ObjectBase):
+            fields = {
+                'test': {
+                    'validate': lambda v: v
+                },
+            }
+
+        list_type = types.List(types.Custom(TestAPI))
+        self.assertIsNone(list_type.validate(None))
+
+        value = [{'test': 'test_value'}]
+        value = list_type.validate(value)
+        self.assertIsInstance(value, list)
+        self.assertIsInstance(value[0], TestAPI)
+        self.assertEqual({'test': 'test_value'}, value[0].as_dict())
