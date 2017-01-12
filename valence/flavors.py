b54e8774ed7b33c0a4b3784553d9cbab7a382fa4
Nate Potter <nathaniel.potter@intel.com>
2017-01-11 16:08:22 -0800

    Implement flavors
    This patch implements flavors, allowing users to save composition
    requirements to the database to be used at any time. Parameters
    included are the flavor's name, RAM, number of CPU cores, and
    processor model.
    
    Change-Id: I356ca9162559598bf1415d2c3b151596f111ac0c
    Implements: blueprint flavor

---
+++ valence/flavors.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2016 Intel, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import logging
+from six.moves import http_client
+
+from valence.common import utils
+from valence.db import api as db_api
+
+LOG = logging.getLogger(__name__)
+
+
+def list_flavors():
+    flavor_models = db_api.Connection.list_flavors()
+    flavors = []
+    for flavor in flavor_models:
+        flavors.append(flavor.as_dict())
+    return utils.make_response(http_client.OK, flavors)
+
+
+def create_flavor(values):
+    flavor = db_api.Connection.create_flavor(values)
+    return utils.make_response(http_client.OK, flavor.as_dict())
+
+
+def delete_flavor(flavorid):
+    db_api.Connection.delete_flavor(flavorid)
+    return "Deleted flavor {0}".format(flavorid)
+
+
+def update_flavor(flavorid, values):
+    flavor = db_api.Connection.update_flavor(flavorid, values)
+    return utils.make_response(http_client.OK, flavor.as_dict())
