880766212e3cfb76c763215829635fdb8da9d608
Lin Yang <lin.a.yang@intel.com>
2017-01-13 19:18:55 -0800

    Use node uuid instead of index for /node(s) api
    Since valence will support multiply backend podm, obviously the node
    index is not unique from different podm, assign uuid for each
    composed node and store node info into etcd. User should use uuid
    instead of index for node operation.
    
    And in some cases, user or other service directly operate composed,
    which will result in inconsistent status between RSD and etcd. So
    check it when get node details, and update if necessary.
    
    Change-Id: Ie365d61083183fa0b699ce978aaa5fdeb740f11d
    Closes-Bug: #1656453

--- valence/tests/unit/db/utils.py
+++ valence/tests/unit/db/utils.py
@@ -56,3 +56,45 @@ def get_test_podmanager(**kwargs):
         'created_at': kwargs.get('created_at', '2016-01-01 00:00:00 UTC'),
         'updated_at': kwargs.get('updated_at', '2016-01-01 00:00:00 UTC'),
     }
+
+
+def get_test_composed_node(**kwargs):
+    return {
+        'uuid': kwargs.get('uuid', 'ea8e2a25-2901-438d-8157-de7ffd68d051'),
+        'name': kwargs.get('name', 'fake_name'),
+        'description': kwargs.get('description', 'fake_description'),
+        'boot_source': kwargs.get('boot_source', 'Hdd'),
+        'health_status': kwargs.get('health_status', 'OK'),
+        'index': kwargs.get('index', '1'),
+        'node_power_state': kwargs.get('node_power_state', 'On'),
+        'node_state': kwargs.get('node_state', 'Assembling'),
+        'pooled_group_id': kwargs.get('bookmark_link', 'None'),
+        'target_boot_source': kwargs.get('target_boot_source', 'Hdd'),
+        'links': kwargs.get(
+            'links',
+            [{'href': 'http://127.0.0.1:8181/v1/nodes/'
+                      '7be5bc10-dcdf-11e6-bd86-934bc6947c55/',
+              'rel': 'self'},
+             {'href': 'http://127.0.0.1:8181/nodes/'
+                      '7be5bc10-dcdf-11e6-bd86-934bc6947c55/',
+              'rel': 'bookmark'}]),
+        'metadata': kwargs.get(
+            'metadata',
+            {'memory': [{'data_width_bit': 0,
+                         'speed_mhz': 2400,
+                         'total_memory_mb': 8192}],
+             'network': [{'ipv4': [{'address': '192.168.0.10',
+                                    'gateway': '192.168.0.1',
+                                    'subnet_mask': '255.255.252.0'}],
+                          'mac': 'e9:47:d3:60:64:66',
+                          'speed_mbps': 0,
+                          'status': 'Enabled',
+                          'vlans': [{'status': 'Enabled',
+                                     'vlanid': 99}]}],
+             'processor': [{'instruction_set': None,
+                            'model': None,
+                            'speed_mhz': 3700,
+                            'total_core': 0}]}),
+        'created_at': kwargs.get('created_at', '2016-01-01 00:00:00 UTC'),
+        'updated_at': kwargs.get('updated_at', '2016-01-01 00:00:00 UTC'),
+    }
