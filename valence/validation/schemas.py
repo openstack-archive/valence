e0efd43d242cd852fbc12feaf07d7524f5970654
Anusha Ramineni <anusha.ramineni@nectechnologies.in>
2017-03-21 16:25:52 +0530

    Add validation for podmanager API
    Partially-Implements: blueprint validation
    Change-Id: I5d313926c1ab0eddc50d24ff30132e849fb1aa28

--- valence/validation/schemas.py
+++ valence/validation/schemas.py
@@ -32,6 +32,43 @@ flavor_schema = {
     'additionalProperties': False,
 }
 
-
 jsonschema.Draft4Validator.check_schema(flavor_schema)
-SCHEMAS = {'flavor_schema': flavor_schema}
+
+podmanager_schema = {
+    'type': 'object',
+    'properties': {
+        'name': {'type': 'string'},
+        'url': {
+            'type': 'string',
+            'format': 'uri',
+            },
+        'authentication': {
+            'type': 'array',
+            'minItems': 1,
+            'items': {
+                'type': 'object',
+                'properties': {
+                    'type': {'type': 'string'},
+                    'auth_items': {
+                        'type': 'object',
+                        'properties': {
+                            'username': {'type': 'string'},
+                            'password': {'type': 'string'},
+                        },
+                        'required': ['username', 'password'],
+                        'additionalProperties': False,
+                    },
+                },
+                'required': ['type', 'auth_items'],
+                'additionalProperties': False,
+            },
+        },
+    },
+    'required': ['name', 'url', 'authentication'],
+    'additionalProperties': False,
+}
+
+
+jsonschema.Draft4Validator.check_schema(podmanager_schema)
+SCHEMAS = {'flavor_schema': flavor_schema,
+           'podmanager_schema': podmanager_schema, }
