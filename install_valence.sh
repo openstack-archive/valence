c5b7611be7d45347c36d0059259a850d2e8f443c
jinxingfang <fang.jinxing@99cloud.net>
2017-07-18 17:17:03 +0800

    optimize valence install script
    Optimize the valence installation script to support on CentOS and
    Ubuntu opertaion system.
    
    Currently, just optimize script to support install valence on Ubuntu.
    Install valence on CentOS need systemctl service script, it will
    submmit in another patch.
    
    Change-Id: Id012251088b532d2073586d5ea0ace8fd67236e3

--- install_valence.sh
+++ install_valence.sh
@@ -1,117 +1,177 @@
 #!/bin/bash
 #title           :install_valence.sh
-#description     :This script will install valence package and deploys conf files
-#author          :Intel Corporation
-#date            :17-10-2016
-#version         :0.1
+#description     :This script will install valence package and deploys conf files on
+#                 centos or ubuntu
+#author          :Intel,99Cloud
+#date            :14-10-2017
+#version         :0.2
 #usage           :sudo -E bash install_valence.sh
 #notes           :Run this script as sudo user and not as root.
 #                 This script is needed still valence is packaged in to .deb/.rpm
 #==============================================================================
 
-install_log=install_valence.log
+INSTALL_LOG="/var/log/valence/valence_install.log"
+OPERATION_SYSTEM="centos"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CURR_USER="$(whoami)"
 
-cd "$DIR"
-echo "Current directory: $DIR" >> $install_log
-if [ "$CURR_USER" != 'root' ]; then
-    echo "You must be root to install."
-    exit
-fi
+function check_os()
+{
+    if [ `cat /proc/version |grep -i ubuntu |wc -l` -gt 0 ];then
+        OPERATION_SYSTEM='ubuntu'
+    elif [ `cat /proc/version |grep -i centos |wc -l` -gt 0 ];then
+        OPERATION_SYSTEM="centos"
+    fi
+    if [ "$CURR_USER" != 'root' ]; then
+        echo "You must be root to install."
+        exit
+    fi
+}
 
-PYHOME="/usr/local/bin"
-echo "Detected PYTHON HOME: $PYHOME" >> $install_log
+function install_dependency_package()
+{
+    check_os
+    echo current os is $OPERATION_SYSTEM
+    if [ $OPERATION_SYSTEM = "ubuntu" ];then
+        echo '--------------'
+        apt update -y
+        apt install -y python-pip wget git
+    elif [ $OPERATION_SYSTEM = "centos" ];then
+        yum install -y epel-release
+        yum install -y python-pip wget git
+    fi
+}
 
-# Copy the config files
-echo "Setting up valence config" >> $install_log
-sed "s/\${CHUID}/$CURR_USER/"  "$DIR"/etc/services-startup-conf/valence.conf > /tmp/valence.conf
-# Use alternate sed delimiter because path will have /
-sed -i "s#PYHOME#$PYHOME#" /tmp/valence.conf
-mv /tmp/valence.conf /etc/init/valence.conf
-# Valence service file required at this path for starting service using systemctl
-if [ -d "/etc/systemd/system" ]; then
-    cp "$DIR"/etc/services-startup-conf/valence.service /etc/systemd/system/valence.service
-fi
+# create_or_delete_etcd mkdir: create database
+# create_or_delete_etcd "rm -rf": delelte database
+function create_or_delete_etcd()
+{
+    etcdctl --endpoints=127.0.0.1:2379 $1 /nodes
+    etcdctl --endpoints=127.0.0.1:2379 $1 /flavors
+    etcdctl --endpoints=127.0.0.1:2379 $1 /pod_managers
+}
 
-# Generate initial sample config file.
-echo "Generating sample config file" >> $install_log
-pip install tox
-tox -egenconfig
+function start_etcd()
+{
+    service etcd start
+    ETCD_STATUS=$?
+    sleep 2
+    timeout=30
+    attempt=1
+    until [[ $ETCD_STATUS = 0 ]]
+    do
+        service etcd status
+        ETCD_STATUS=$?
+        sleep 1
+        attempt=$((attempt+1))
+        if [[ $attempt -eq timeout ]];then
+            echo "Database failed to start, aborting installation."
+            create_or_delete_etcd "rm -rf"
+            rm -rf /var/etcd /usr/local/bin/etcd /usr/local/bin/etcdctl
+            exit
+        fi
+    done
+}
 
-# create conf directory for valence if it doesn't exist
-if [ ! -d "/etc/valence" ]; then
-    mkdir /etc/valence
-fi
-chown "$CURR_USER":"$CURR_USER" /etc/valence
-VALENCE_CONF=/etc/valence/valence.conf
-cp "$DIR"/etc/valence.conf.sample /etc/valence/valence.conf
+function install_etcd()
+{
+    echo "0:------+++++++++++"
+    ETCD_VER=v3.1.2
+    DOWNLOAD_URL=https://github.com/coreos/etcd/releases/download
+    echo "1:------+++++++++++"
+    echo "etcd-${ETCD_VER}-linux-amd64.tar.gz"
+    echo `ls -h /tmp`
+    if [ ! -f /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz ];then
+        echo "Downloading etcd into /tmp from ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz" >> $INSTALL_LOG
+        curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz -o /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
+        if [ $? -ne 0 ];then
+            echo "Downloading etcd into /tmp from ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz failed!!!"
+            rm -f /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
+            echo "Downloading etcd into /tmp from ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz failed!!!" >> $INSTALL_LOG
+            exit
+        fi
+    else
+        echo "2:------+++++++++++"
+        echo "etcd-${ETCD_VER}-linux-amd64.tar.gz already exist in /tmp"
+    fi
+    echo "3:------+++++++++++"
+    mkdir -p /var/etcd && tar xzvf /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz -C /var/etcd --strip-components=1
+    chown "$CURR_USER":"$CURR_USER" /var/etcd
+    mv /var/etcd/etcd /usr/local/bin/etcd && mv /var/etcd/etcdctl /usr/local/bin/etcdctl
+    # Etcd service file required at this path for starting service using systemctl
 
-sudo sed -i "s/#debug\s*=.*/debug=true/" $VALENCE_CONF
-sudo sed -i "s/#log_level\s*=.*/log_level=debug/" $VALENCE_CONF
-sudo sed -i "s/#log_file\s*=.*/log_file=/var/log/valence/valence.log/" $VALENCE_CONF
-sudo sed -i "s/#bind_host\s*=.*/bind_host=0.0.0.0/" $VALENCE_CONF
-sudo sed -i "s/#bind_port\s*=.*/bind_port=8181/" $VALENCE_CONF
-sudo sed -i "s/#timeout\s*=.*/timeout=1000/" $VALENCE_CONF
-sudo sed -i "s/#workers\s*=.*/workers=4/" $VALENCE_CONF
-sudo sed -i "s/#host\s*=.*/host=localhost/" $VALENCE_CONF
-sudo sed -i "s/#port\s*=.*/port=2379/" $VALENCE_CONF
+    echo "------+++++++++++"
+    if [ $OPERATION_SYSTEM = "ubuntu" ];then
+        if [ -d "/etc/systemd/system" ];then
+            cp "$DIR"/etc/services-startup-conf/etcd.service /etc/systemd/system/etcd.service
+            sed "s/\${CHUID}/$CURR_USER/"  "$DIR"/etc/services-startup-conf/etcd.conf > /etc/init/etcd.conf
+        fi
+    elif [ $OPERATION_SYSTEM = "centos" ];then
+        # TODO: support centos
+        echo "------"
+    fi
+    start_etcd
+}
 
-# create log directory for valence if it doesn't exist
-if [ ! -d "/var/log/valence" ]; then
-    mkdir /var/log/valence
-fi
-chown "$CURR_USER":"$CURR_USER" /var/log/valence
+function prepare_install_valence()
+{
+    CURR_USER="$(whoami)"
+    if [ ! -d '/var/log/valence' ];then
+        mkdir -p /var/log/valence
+        chown $CURR_USER:$CURR_USER /var/log/valence
+        touch /var/log/valence/valence_install.log
+    fi
+    if [ ! -d '/etc/valence/' ];then
+        mkdir -p /etc/valence
+        chown $CURR_USER:$CURR_USER /etc/valence/
+    fi
+    INSTALL_LOG=/var/log/valence/valence_install.log
+    echo "Current directory: $DIR" >> $INSTALL_LOG
+}
 
-echo "Installing dependencies from requirements.txt" >> $install_log
-pip install -r requirements.txt
+function install_valence()
+{
+    pip install -r requirements.txt
+    python setup.py install
+    if [ $? -ne 0 ]; then
+        echo "ERROR: setup.py failed. Please check /var/log/valence/INSTALL_LOG for details.
+              Please fix the error and retry."
+        exit
+    fi
 
-echo "Installing the etcd database" >> $install_log
-ETCD_VER=v3.1.2
-DOWNLOAD_URL=https://github.com/coreos/etcd/releases/download
-curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz -o /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
-mkdir -p /var/etcd && tar xzvf /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz -C /var/etcd --strip-components=1
-chown "$CURR_USER":"$CURR_USER" /var/etcd
-mv /var/etcd/etcd /usr/local/bin/etcd && mv /var/etcd/etcdctl /usr/local/bin/etcdctl
-# Etcd service file required at this path for starting service using systemctl
-if [ -d "/etc/systemd/system" ]; then
-    cp "$DIR"/etc/services-startup-conf/etcd.service /etc/systemd/system/etcd.service
-fi
+    if [ $OPERATION_SYSTEM = "ubuntu" ];then
+        if [ -d "/etc/systemd/system" ]; then
+            cp "$DIR"/etc/services-startup-conf/valence.service /etc/systemd/system/valence.service
+        fi
+    elif [ $OPERATION_SYSTEM = "centos" ];then
+        # TODO: support centos
+        echo ""
+    fi
 
-sed "s/\${CHUID}/$CURR_USER/"  "$DIR"/etc/services-startup-conf/etcd.conf > /etc/init/etcd.conf
+}
 
-echo "Starting etcd database" >> $install_log
-service etcd start
-ETCD_STATUS=$?
-sleep 2
-timeout=30
-attempt=1
-until [[ $ETCD_STATUS = 0 ]]
-do
-  service etcd status
-  ETCD_STATUS=$?
-  sleep 1
-  attempt=$((attempt+1))
-  if [[ $attempt -eq timeout ]]
-  then
-    echo "Database failed to start, aborting installation."
-    rm -rf /var/etcd /usr/local/bin/etcd /usr/local/bin/etcdctl
-    exit
-  fi
-done
+function config_valence()
+{
+    pip install tox
+    echo "It will take few minites to generate configuration file by tox tool, please be patient!!!"
+    tox -egenconfig
+    VALENCE_CONF=/etc/valence/valence.conf
+    cp "$DIR"/etc/valence.conf.sample /etc/valence/valence.conf
+    chown $CURR_USER:$CURR_USER /etc/valence/
+    sudo sed -i "s/#debug\s*=.*/debug=true/" $VALENCE_CONF
+    sudo sed -i "s/#log_level\s*=.*/log_level=debug/" $VALENCE_CONF
+    sudo sed -i "s/#log_file\s*=.*/log_file=\/var\/log\/valence\/valence.log/" $VALENCE_CONF
+    sudo sed -i "s/#bind_host\s*=.*/bind_host=0.0.0.0/" $VALENCE_CONF
+    sudo sed -i "s/#bind_port\s*=.*/bind_port=8181/" $VALENCE_CONF
+    sudo sed -i "s/#timeout\s*=.*/timeout=1000/" $VALENCE_CONF
+    sudo sed -i "s/#workers\s*=.*/workers=4/" $VALENCE_CONF
+    sudo sed -i "s/#host\s*=.*/host=localhost/" $VALENCE_CONF
+    sudo sed -i "s/#port\s*=.*/port=2379/" $VALENCE_CONF
+}
 
-echo "Adding database directories" >> $install_log
-etcdctl --endpoints=127.0.0.1:2379 mkdir /nodes
-etcdctl --endpoints=127.0.0.1:2379 mkdir /flavors
-etcdctl --endpoints=127.0.0.1:2379 mkdir /pod_managers
 
-echo "Invoking setup.py" >> $install_log
-python setup.py install
-if [ $? -ne 0 ]; then
-    echo "ERROR: setup.py failed. Please check $install_log for details.
-          Please fix the error and retry."
-    exit
-fi
-
-echo "Installation Completed"
-echo "To start valence : sudo service valence start"
+install_dependency_package
+install_etcd
+prepare_install_valence
+install_valence
+config_valence
