1f67977d383bf83516d23e322c24c3cd26fb503e
Anusha Ramineni <anusha.ramineni@nectechnologies.in>
2017-08-28 14:50:29 +0530

    Add new 'device' DB table to valence
    This commit adds new model 'Device' to store the pooled resources
    info in the DB.
    
    Partially-Implements: blueprint add-device-orchestration
    
    Change-Id: Ic93caaede23b3924d5897b2cb4583e27a1072938

--- valence/db/models.py
+++ valence/db/models.py
@@ -187,7 +187,7 @@ class Flavor(ModelBaseWithTimeStamp):
                 'validate': types.Dict.validate
             },
             'validate': types.Dict.validate
-        }
+        },
     }
 
 
@@ -212,6 +212,41 @@ class ComposedNode(ModelBaseWithTimeStamp):
             'validate': types.Text.validate
         },
         'managed_by': {
+            'validate': types.Text.validate
+        }
+    }
+
+
+class Device(ModelBaseWithTimeStamp):
+
+    path = "/devices"
+
+    fields = {
+        'uuid': {
+            'validate': types.Text.validate
+        },
+        'podm_id': {
+            'validate': types.Text.validate
+        },
+        'node_id': {
+            'validate': types.Text.validate
+        },
+        'type': {
+            'validate': types.Text.validate
+        },
+        'pooled_group_id': {
+            'validate': types.Text.validate
+        },
+        'state': {
+            'validate': types.Text.validate
+        },
+        'properties': {
+            'validate': types.List(types.Dict).validate
+        },
+        'extra': {
+            'validate': types.List(types.Dict).validate
+        },
+        'resource_uri': {
             'validate': types.Text.validate
         }
     }
